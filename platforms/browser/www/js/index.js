259350bbd83e0ae3fb21321a0fad991a0597d769
Sahan Lamahewa <sahan.lamahewa@gmail.com>
2018-03-16 15:06:07 +0530

    first commit

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -45,6 +45,29 @@ document.getElementById("audioCapture").addEventListener("click", audioCapture);
             text: 'Thats pretty easy...',
             foreground: true
       });
+
+      navigator.getUserMedia = navigator.getUserMedia ||
+                               navigator.webkitGetUserMedia ||
+                               navigator.mozGetUserMedia;
+
+      if (navigator.getUserMedia) {
+         navigator.getUserMedia({ audio: true, video: true },
+            function(stream) {
+               var video = document.querySelector('video');
+               video.srcObject = stream;
+               video.onloadedmetadata = function(e) {
+                 video.play();
+               };
+            },
+            function(err) {
+               console.log("The following error occurred: " + err.name);
+            }
+         );
+      } else {
+         console.log("getUserMedia not supported");
+      }
+
+
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
