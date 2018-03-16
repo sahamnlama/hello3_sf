4ce07c438db07e4792493b00e9bbcb259c6ed69a
Sahan Lamahewa <sahan.lamahewa@gmail.com>
2018-03-16 15:42:14 +0530

    first commit

--- www/js/index.js
+++ www/js/index.js
@@ -1,70 +1,19 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+function onLoad() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
 
-        app.receivedEvent('deviceready');
-        //alert('ready');
-//        alert(Media);
-//alert(navigator.getUserMedia);
+// device APIs are available
+//
+function onDeviceReady() {
+  document.getElementById("audioCapture").addEventListener("click", audioCapture);
+  document.getElementById("playAudio").addEventListener("click", playAudio);
 
-document.getElementById("audioCapture").addEventListener("click", audioCapture);
-document.getElementById("playAudio").addEventListener("click", playAudio);
-
-        cordova.plugins.notification.local.schedule({
-            title: 'My first notification',
-            text: 'Thats pretty easy...',
-            foreground: true
-      });
-
-
-
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-
-
-};
+          cordova.plugins.notification.local.schedule({
+              title: 'My first notification',
+              text: 'Thats pretty easy...',
+              foreground: true
+        });
+}
 
 
 function playAudio() {
